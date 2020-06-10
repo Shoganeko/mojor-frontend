d54bbfa54b6ccf00af9f448e94dae7cd4022e614
sho <blaziyt@gmail.com>
2020-06-10 11:05:51 -0500

    Change to production URL

---
+++ src/pages/Account.js
@@ -0,0 +1,22 @@
+import { useRouteMatch } from "react-router-dom"
+import React from "react"
+
+export default function Account() {
+    const {
+        params: { user }
+    } = useRouteMatch()
+
+    return (<AccountPage name={user} />)
+}
+
+class AccountPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+
+
+        this.state = {
+            name
+        }
+    }
+}
